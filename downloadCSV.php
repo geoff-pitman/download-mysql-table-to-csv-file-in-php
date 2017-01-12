6d15da9b523c29df63d947e970c9032879967151
Geoffrey Pitman <gpitman379@gmail.com>
2017-01-12 15:44:15 -0500

    Create downloadCSV.php

---
+++ downloadCSV.php
@@ -0,0 +1,31 @@
+<?php
+// let client's browser know to expect text/csv file attachment
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// connect to database
+$conn = mysql_connect('localhost', 'gpitm379', 'bigboom');
+mysql_select_db('gpitm379_bookstore');
+
+// create file pointer connected to output stream
+$output = fopen('php://output', 'w');
+
+// get table data
+$rows = mysql_query('SELECT * FROM Registration');
+
+// get column names
+$field = mysql_num_fields($rows);  
+for ( $i = 0; $i < $field; $i++ ) 
+   $names[] = mysql_field_name( $rows, $i );
+
+// output column names
+fputcsv($output, $names);
+     
+// output table data
+while ($row = mysql_fetch_assoc($rows))
+    fputcsv($output, $row); // possible to change delimiter/enclosure
+
+// clean up
+mysql_close($conn);
+fclose($output);
+?>
