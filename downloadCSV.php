5b61454b0637862062c17dc6d12be27267f051f7
Geoffrey Pitman <gpitman379@gmail.com>
2017-01-12 15:45:59 -0500

    Update downloadCSV.php

--- downloadCSV.php
+++ downloadCSV.php
@@ -4,8 +4,8 @@ header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 
 // connect to database
-$conn = mysql_connect('localhost', 'gpitm379', 'bigboom');
-mysql_select_db('gpitm379_bookstore');
+$conn = mysql_connect('localhost', <username> , <password>);
+mysql_select_db(<DBname>);
 
 // create file pointer connected to output stream
 $output = fopen('php://output', 'w');
